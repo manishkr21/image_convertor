df60eca51b5c48bb6f93e1f6eb83e823c03023a1
manish kumar <manishk21@iitk.ac.in>
2024-02-21 11:09:24 +0530

    intial Commit

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# image_convertor
+# Image Converter
+
+Web Image Converter is a web-based tool for converting images from one format to another directly in your browser.
+
+## Features
+
+- Convert images between different formats such as JPEG, PNG, grayscale, ...
+- Simple and intuitive user interface accessible from any modern web browser.
+- Supports drag-and-drop functionality for easy uploading of images.
+- Provides real-time conversion preview and download options.
+
+## Demo
+
+![Web Image Converter Demo](demo.gif)
+
+Converted images
+![converted PNG image](demo.png)
+
+## Installation
+
+Web Image Converter is a web application and does not require any installation. Simply access the application through a web browser.
+
+## Usage
+
+1. Open your web browser and navigate to the URL where the Web Image Converter is hosted.
+2. Drag and drop your image files onto the web interface or click the upload button to select images from your computer.
+3. Choose the desired output format for conversion.
+4. Click the "Submit" button to initiate the conversion process.
+5. Once the conversion is complete, download the converted images to your computer.
+
+## Technologies Used
+
+- HTML/CSS/JavaScript for the front-end interface.
+- [Flask](https://flask.palletsprojects.com/) framework for the back-end server.
+- [OpenCV](https://opencv.org/) library for image processing.
+
+## Development
+
+To set up the development environment:
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/manishk21/image_converter.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd image_editor
+    ```
+
+3. Install the required dependencies:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4. Run the Flask server:
+
+    ```bash
+    python main.py
+    ```
+
+5. Access the application at `http://localhost:8000` in your web browser.
+
+## Contributing
+
+Contributions are welcome! Please fork the repository and submit a pull request with your changes.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ main.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template , request , flash , redirect , url_for
+from werkzeug.utils import secure_filename 
+import os
+import cv2
+import numpy as np
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def process_image(filename, operation):
+    print('Processing image', filename, 'with operation', operation)
+    img = cv2.imread(f'static/uploads/{filename}')
+    print('Image shape:', img.shape)
+    match operation:
+        case 'cwebp':
+            newFilename = f'static/processed/cwebp_{filename.split(".")[0]}.webp'
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case 'cjpg':
+            newFilename = f'static/processed/cjpg_{filename.split(".")[0]}.jpg'
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case 'cpng':
+            newFilename = f'static/processed/cpng_{filename.split(".")[0]}.png'
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case 'csvg':
+            newFilename = f'static/processed/csvg_{filename.split(".")[0]}.svg'
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case 'cgrey':
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            newFilename = f'static/processed/cgrey_{filename}'
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+        case 'cblur':
+            imgProcessed = cv2.GaussianBlur(img, (21, 21), 0)
+            newFilename = f'static/processed/cblur_{filename}'
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+
+        case 'ccanny':
+            imgProcessed = cv2.Canny(img, 100, 200)
+            newFilename = f'static/processed/ccanny_{filename}'
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+
+        case 'cdilate':
+            kernel = np.ones((5, 5), np.uint8)
+            imgProcessed = cv2.dilate(img, kernel, iterations=1)
+            newFilename = f'static/processed/cdilate_{filename}'
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+
+        case 'cerode':
+            kernel = np.ones((5, 5), np.uint8)
+            imgProcessed = cv2.erode(img, kernel, iterations=1)
+            newFilename = f'static/processed/cerode_{filename}'
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+
+        case 'cThreshold':
+            imgProcessed = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY)
+            newFilename = f'static/processed/cThreshold_{filename}'
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+
+        case _:
+            print('Invalid operation', operation)
+            return redirect(url_for('error'))
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == 'POST':
+        # Do something with the form data
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return render_template('error.html')
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return render_template('error.html')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'uploads/' + filename))
+            op = request.form['operation']
+            newFilename = process_image(filename, op)
+
+            flash(f'File uploaded and processed <a href="{newFilename}" target="_blank"> here </a>')
+            return render_template('index.html')
+        return 'Form data posted'
+    return render_template('edit.html')
+
+app.secret_key = '535345464646474634653454364'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+
+app.run(debug=True, port=8000, host='0.0.0.0')
